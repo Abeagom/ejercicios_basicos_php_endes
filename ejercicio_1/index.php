ad231d75bfb813142ae1c6ff35a609ffa896f103
Abeagom <beas.gomez.alberto@iescamas.es>
2024-10-12 20:59:03 +0200

    Add files via upload

---
+++ ejercicio_1/index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Ejercicio 1</title>
+</head>
+<body>
+<h1>Ejercicio 1: Saludo Personalizado </h1>
+<h3>Escribe tu nombre y apellido</h3>
+<form action="control.php" method="get">
+Nombre: <input type="text" name="nombre"><br>
+Apellido: <input type="text" name="apellido">
+<input type="submit" value="enviar">
+</form>
+</body>
+</html>
